7d5756610bee78e342e8787b03d8dda7ea69671d
tc-imba <liuyh615@sjtu.edu.cn>
2018-09-07 12:32:56 +0800

    update arglist

--- tc4/arglist.py
+++ tc4/arglist.py
@@ -1,8 +1,22 @@
 class ArgList:
-    def __init__(self):
-        self.__args = []
+    def __init__(self, main=''):
+        self.__main = main
+        self.__data = []
+        self.__args_loaded = False
+        self.__args = [self.__main]
 
-    def add(self, name, value=None, replace=False):
+    def add(self, data, replace=False):
+        if isinstance(data, str):
+            data = data.split(' ')
+        if not len(data):
+            return
+        if data[0].startswith('-'):
+            name = data[0]
+            value = data[1:]
+        else:
+            name = None
+            value = data
+
         if not value:
             value = name
             name = None
@@ -15,22 +29,30 @@ class ArgList:
         else:
             value = list(value)
         if replace and name:
-            for i, (_name, _value) in enumerate(self.__args):
+            for i, (_name, _value) in enumerate(self.__data):
                 if _name == name:
-                    self.__args[i][1] = value
+                    self.__data[i][1] = value
                     return
-        self.__args.append((name, value))
+        self.__data.append((name, value))
+        self.__args_loaded = False
 
     def set(self, name, value):
         self.add(name, value, True)
-        pass
+
+    def set_main(self, main):
+        self.__args[0] = self.__main = main
+
+    def get_main(self):
+        return self.__main
 
     def get_args(self):
-        args = []
-        for arg in self.__args:
-            arg[0] and args.append(arg[0])
-            arg[1] and args.extend(arg[1])
-        return args
+        if not self.__args_loaded:
+            self.__args = [self.__main]
+            for arg in self.__data:
+                arg[0] and self.__args.append(arg[0])
+                arg[1] and self.__args.extend(arg[1])
+            self.__args_loaded = True
+        return self.__args
 
     def serialize(self):
         return ' '.join(self.get_args())
@@ -38,7 +60,7 @@ class ArgList:
 
 # test
 if __name__ == '__main__':
-    arg_list = ArgList()
+    arg_list = ArgList('g++')
     arg_list.set('-o', 'main')
     arg_list.add(['main.cpp', '1.cpp'])
     print(arg_list.serialize())
--- tc4/toolchain.py
+++ tc4/toolchain.py
@@ -1,3 +1,59 @@
+from copy import copy, deepcopy
+from os import path
+from ruamel import yaml
+
+from tc4.arglist import ArgList
+
+
+def load_config(filename=None):
+    global config
+    if not filename:
+        filename = path.join(path.dirname(__file__), 'toolchain.yaml')
+    pass
+    with open(filename) as file:
+        data = yaml.safe_load(file)
+        __config = dict()
+        for name, tool in data:
+            if __config.__contains__(name):
+                raise ValueError('Tool %s redefined' % name) from None
+            __config[name] = Tool(name, tool)
+        config = __config
+
+
+def get_config():
+    if not config:
+        load_config()
+    return config
+
+
+class Tool:
+    def __init__(self, name, data):
+        if 'type' not in data:
+            raise ValueError('Type undefined ') from None
+        # self.type = 'type' in data and data['type'] or 'system'
+        self.name = name
+        self.extends = 'base'
+        for key, value in data:
+            self.__setattr__(key, value)
+        if hasattr(self, 'execute_args'):
+            self.__execute_args = ArgList(self.execute_args[0])
+            self.add_args(self.execute_args[1:])
+
+    def __getattribute__(self, item):
+        if item == 'execute_args':
+            return self.__execute_args
+        else:
+            return self.__dict__[item]
+
+    def extend(self, data):
+        self.extends = 'extends' in data and data['extends'] or 'base'
+        pass
+
+    def add_args(self, execute_args):
+        for arg in execute_args:
+            self.__execute_args.add(arg)
+
+
 class ToolChain:
     def __init__(self, problem, name, data):
         self.__problem = problem
@@ -23,6 +79,10 @@ class ToolChain:
         self.__load_tools()
         self.__tools[key] = value
         return self.__tools[key]
+
+    def __copy__(self):
+        obj = ToolChain(self.__problem, self.__name, [])
+        obj.__data = copy(self.__data)
 
     def add_tool(self, tool):
         if 'type' not in tool:
